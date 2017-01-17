34f4d3f88af496be46a7cbe1a3fb5a85a8627967
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-01-17 07:27:29 -0800

    Add countdown feature

--- public/client/factories/factories.js
+++ public/client/factories/factories.js
@@ -3,8 +3,13 @@ angular.module('chickfilApp')
     var getMenu = function() {
       return $http.get('/api/fetchMenu');
     };
+
+    var getTimeLeft = function() {
+      return moment("2017-01-21T12:00:00.000").countdown().toString();
+    };
     
     return {
-      getMenu: getMenu
+      getMenu: getMenu,
+      getTimeLeft: getTimeLeft
     };
   });
--- public/client/orders/ordersCtrl.js
+++ public/client/orders/ordersCtrl.js
@@ -6,21 +6,30 @@ angular.module('chickfilApp')
     $scope.sodas = [];
     $scope.milkshakes = [];
     $scope.sauces = [];
+    $scope.userOrder = [];
+    $scope.countdown = ordersFactory.getTimeLeft();
+    setInterval(function() {
+      $scope.countdown = ordersFactory.getTimeLeft();
+      $scope.$digest();
+    }, 1000);
 
     ordersFactory.getMenu().then(function(res) {
       $scope.menu = res.data;
       $scope.menu.forEach(function(item) {
         if(item.entree) {
           $scope.entrees.push(item);
-          item.prices = [item.price];
+          item.prices = [{cost: item.price, user: item}];
         } else if(item.schemaName.indexOf('drink') !== -1) {
           $scope.sodas.push(item);
-          item.prices = [item.small, item.medium, item.large];
+          item.prices = [{cost: item.small, user: item},
+                          {cost: item.medium, user: item},
+                          {cost: item.large, user: item}];
         } else if(item.schemaName.indexOf('shake') != -1) {
           $scope.milkshakes.push(item);
-          item.prices = [item.small, item.large];
+          item.prices = [{cost: item.small, user: item}, {cost: item.large, user: item}];
         } else if(item.schemaName.indexOf('sauce') != -1) {
-          $scope.sauces.push(item);
+          $scope.sauces.push([{numOfSauces: 1, user: item}, {numOfSauces: 2, user: item}, {numOfSauces: 3, user: item}, 
+                              {numOfSauces: 4, user: item}, {numOfSauces: 5, user: item}]);
         }
       });
 
@@ -29,11 +38,14 @@ angular.module('chickfilApp')
     });
 
     $scope.addToTotal = function(drinkPrice) {
-      $scope.total = parseFloat(($scope.total + drinkPrice).toPrecision(3));
+      $scope.total = parseFloat(($scope.total + drinkPrice.cost).toPrecision(3));
+      $scope.userOrder.push(drinkPrice);
     }
 
     $scope.submitOrder = function() {
+      var user = {user: $scope.user, userOrder: $scope.userOrder}
       $http.post('/api/users', {user: $scope.user}).then(function(res) {
+        console.log('Post to /api/users successful');
       }, function(err) {
         console.log('err', err);
       });
